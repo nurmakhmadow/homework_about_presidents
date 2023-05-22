37e415e169741d1bfe850a38a07705c148df240e
nurmakhmadow <nurmakhmadow@mail.ru>
2023-05-22 00:52:37 -0500

    without changes

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let name = prompt('Как вас зовут?').toLowerCase().trim()
+
+
+if (name === "putin" || name ==="zelenskiy"  || name ==="biden"  || name ==="trump" ) {
+    console.log('welcome mr.president ' + name );
+} else {
+    console.log('only for Presidents');
+}
+
